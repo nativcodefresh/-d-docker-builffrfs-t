32d6048fac8b55c056afb6275904ddd1e1115758
Roy Shmueli <roy@codefresh.io>
2017-09-24 13:48:15 +0300

    Adding new line after warning
    * Fix bug with the new line of warning
    
    * fix test

--- src/registries.js
+++ src/registries.js
@@ -3,6 +3,8 @@
 const Promise = require('bluebird');
 const chalk = require('chalk');
 
+const DOCKER_HUB_REGISTRY_URL = 'https://index.docker.io/v1/';
+
 const command = 'docker';
 
 const { spawn } = require('child_process');
@@ -11,14 +13,20 @@ exports.registryConfigOption = (registries) => {
     return Promise.all(Object.keys(registries))
         .each((domain) => {
             const credentials = registries[domain];
-            const args = ['login', domain, '-u', credentials.username, '-p', credentials.password];
+
+            let args;
+            if (domain === DOCKER_HUB_REGISTRY_URL) {
+                args = ['login', '-u', credentials.username, '-p', credentials.password];
+            } else {
+                args = ['login', domain, '-u', credentials.username, '-p', credentials.password];
+            }
 
             return new Promise((resolve) => {
                 const child = spawn(command, args, { stdio: 'ignore' });
 
-                child.on('exit', (code, stdout, stderr) => {
+                child.on('exit', (code) => {
                     if (code !== 0) {
-                        process.stdout.write(chalk.bold.yellow(`problem connecting to '${domain}': ${stderr}`));
+                        process.stdout.write(chalk.bold.yellow(`problem connecting to '${domain}'\n`));
                     }
                     resolve();
                 });
